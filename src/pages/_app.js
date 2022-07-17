b76499972a8e6621eece31b93a9764ee1d528b2a
Kritix <kristoffer@kritix.dk>
2022-07-18 00:35:20 +0200

    Attempt 4 at fixing animation issue.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,53 +3,55 @@ import Header from "../components/header/header.js";
 import Footer from "../components/footer/footer.js";
 import Transition from "../components/page-ani";
 import Router from "next/router";
-import { useEffect } from 'react';
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export const OPACITY_EXIT_DURATION = 1;
 
 const routeChange = () => {
   const tempFix = () => {
-      const allStyleElems = document.querySelectorAll('style[media="x"]');
-      allStyleElems.forEach((elem) => {
-          elem.removeAttribute('media');
-      });
+    const allStyleElems = document.querySelectorAll('style[media="x"]');
+    allStyleElems.forEach((elem) => {
+      elem.removeAttribute("media");
+    });
   };
   tempFix();
 };
 
 export const useTransitionFix = () => {
   useEffect(() => {
-      Router.events.on('routeChangeComplete', routeChange);
-      Router.events.on('routeChangeStart', routeChange);
+    Router.events.on("routeChangeComplete", routeChange);
+    Router.events.on("routeChangeStart", routeChange);
 
-      return () => {
-          Router.events.off('routeChangeComplete', routeChange);
-          Router.events.off('routeChangeStart', routeChange);
-      };
+    return () => {
+      Router.events.off("routeChangeComplete", routeChange);
+      Router.events.off("routeChangeStart", routeChange);
+    };
   }, []);
 
   useEffect(() => {
-      Router.router?.push(Router.router?.pathname);
+    Router.router?.push(Router.router?.pathname);
   }, []);
 };
 
 const handleRouteChange = () => {
-	const elements = document.querySelectorAll('style[media="x"]');
-	elements.forEach((elem) => elem.removeAttribute("media"));
-	setTimeout(() => {
-		elements.forEach((elem) => elem.remove());
-	}, OPACITY_EXIT_DURATION * 1000);
+  const elements = document.querySelectorAll('style[media="x"]');
+  elements.forEach((elem) => elem.removeAttribute("media"));
+  setTimeout(() => {
+    elements.forEach((elem) => elem.remove());
+  }, OPACITY_EXIT_DURATION * 1000);
 };
 
 Router.events.on("routeChangeStart", handleRouteChange);
 Router.events.on("routeChangeComplete", handleRouteChange);
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <>
       <Header />
       <Transition>
-        <Component {...pageProps} />
+        <Component {...pageProps} key={router.pathname} />
       </Transition>
       <Footer />
     </>
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,8 @@ import Head from "next/head";
 import Image from "next/image";
 import aboutImage from "../images/wipImage.PNG";
 
+//Til brug
+
 const About = () => {
   return (
     <div className={styles.container}>
@@ -20,12 +22,25 @@ const About = () => {
           </p>
           <p className={styles.paragraph}>
             SK-Revision har igennem mange år specialiseret sig i mindre
-            virksomheder indenfor diverse håndværk, kontor,
-            restauration, kundeservice, mm.
+            virksomheder indenfor diverse håndværk, kontor, restauration,
+            kundeservice, mm.
           </p>
+          <p className={styles.paragraph}>
+            Virksomheden er registeret hos FDR, Foreningen Danske Revisorer, som
+            et godkendt revisionsvirksomhed, og Sanne Kjærgaard er registreret
+            revisor.
+          </p>
+
+          <a
+            className={styles.textLink}
+            href="https://fdr.dk/fdr-revisorer/sk-revision/"
+            target="_blank"
+          >
+            {">>"} Se revisionsvirksomhedsregistrering
+          </a>
         </div>
         <div className={styles.image}>
-          <Image src={aboutImage} alt="SK-Revision about"/>
+          <Image src={aboutImage} alt="SK-Revision about" />
         </div>
       </main>
     </div>
