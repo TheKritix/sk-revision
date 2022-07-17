6d3d9fdeeede70ad20f98d8165c573826b269d7d
Kritix <kristoffer@kritix.dk>
2022-07-17 21:14:21 +0200

    Attempt 3

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,35 +5,44 @@ import Transition from "../components/page-ani";
 import Router from "next/router";
 import { useEffect } from 'react';
 
-//Attempt at fixing react-motion bug
 export const OPACITY_EXIT_DURATION = 1;
 
 const routeChange = () => {
-    const tempFix = () => {
-        const elements = document.querySelectorAll('style[media="x"]');
-        elements.forEach((elem) => elem.removeAttribute('media'));
-        setTimeout(() => {
-            elements.forEach((elem) => elem.remove());
-        }, OPACITY_EXIT_DURATION * 1000);
-    };
-    tempFix();
+  const tempFix = () => {
+      const allStyleElems = document.querySelectorAll('style[media="x"]');
+      allStyleElems.forEach((elem) => {
+          elem.removeAttribute('media');
+      });
+  };
+  tempFix();
 };
 
 export const useTransitionFix = () => {
-    useEffect(() => {
-        Router.events.on('routeChangeComplete', routeChange);
-        Router.events.on('routeChangeStart', routeChange);
+  useEffect(() => {
+      Router.events.on('routeChangeComplete', routeChange);
+      Router.events.on('routeChangeStart', routeChange);
 
-        return () => {
-            Router.events.off('routeChangeComplete', routeChange);
-            Router.events.off('routeChangeStart', routeChange);
-        };
-    }, []);
+      return () => {
+          Router.events.off('routeChangeComplete', routeChange);
+          Router.events.off('routeChangeStart', routeChange);
+      };
+  }, []);
 
-    useEffect(() => {
-        Router.router?.push(Router.router?.pathname);
-    }, []);
+  useEffect(() => {
+      Router.router?.push(Router.router?.pathname);
+  }, []);
 };
+
+const handleRouteChange = () => {
+	const elements = document.querySelectorAll('style[media="x"]');
+	elements.forEach((elem) => elem.removeAttribute("media"));
+	setTimeout(() => {
+		elements.forEach((elem) => elem.remove());
+	}, OPACITY_EXIT_DURATION * 1000);
+};
+
+Router.events.on("routeChangeStart", handleRouteChange);
+Router.events.on("routeChangeComplete", handleRouteChange);
 
 function MyApp({ Component, pageProps }) {
   return (
