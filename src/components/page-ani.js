e54537531f59dc14ed095d7076fe75baf92a8e35
Kritix <kristoffer@kritix.dk>
2022-07-21 23:39:43 +0200

    It is what it is

--- src/components/page-ani.js
+++ src/components/page-ani.js
@@ -1,7 +1,7 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
 import Router from "next/router";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 
 const variants = {
   hidden: { opacity: 0, x: -200, y: 0 },
@@ -9,40 +9,57 @@ const variants = {
   exit: { opacity: 0, x: 0, y: -100 },
 };
 
-
+let copies = [];
 
 //https://letsbuildui.dev/articles/animated-page-transitions-in-nextjs
 //https://www.framer.com/docs/introduction/
 const Transition = ({ children }) => {
+  
 
-  let copies = [];
+  useEffect(() => {
+    Router.events.on("beforeHistoryChange", onLoad);
+    return () => {
+      Router.events.off("beforeHistoryChange", onLoad);
+    };
+  }, []);
 
-useEffect(() => {
-  Router.events.on("beforeHistoryChange", onLoad);
-  return () => {
-    Router.events.off("beforeHistoryChange", onLoad);
+  const onLoad = () => {
+    // Create a clone of every <style> and <link> that currently affects the page. It doesn't matter
+    // if Next.js is going to remove them or not since we are going to remove the copies ourselves
+    // later on when the transition finishes.
+    const nodes = document.querySelectorAll(
+      "link[rel=stylesheet], style:not([media=x])"
+    );
+    copies = [...nodes].map((el) => el.cloneNode(true));
+
+    console.log(document.head)
+    console.log(copies)
+    for (let copy of copies) {
+      // Remove Next.js' data attributes so the copies are not removed from the DOM in the route
+      // change process.
+      copy.removeAttribute("data-n-p");
+      copy.removeAttribute("data-n-href");
+      console.log("piss")
+      // Add duplicated nodes to the DOM.
+      
+      document.head.appendChild(copy);
+    }
   };
-}, []);
 
-const onLoad = () => {
-  // Create a clone of every <style> and <link> that currently affects the page. It doesn't matter
-  // if Next.js is going to remove them or not since we are going to remove the copies ourselves
-  // later on when the transition finishes.
-  const nodes = document.querySelectorAll(
-    "link[rel=stylesheet], style:not([media=x])"
-  );
-  copies = [...nodes].map((el) => el.cloneNode(true));
+  const onExit = () => {
+    console.log("shit");
+    console.log(copies);
+    for (let copy of copies) {
+      console.log("no homo");
+      // Remove previous page's styles after the transition has finalized.
+      document.head.removeChild(copy);
+    }
+  };
 
-  for (let copy of copies) {
-    // Remove Next.js' data attributes so the copies are not removed from the DOM in the route
-    // change process.
-    copy.removeAttribute("data-n-p");
-    copy.removeAttribute("data-n-href");
-
-    // Add duplicated nodes to the DOM.
-    document.head.appendChild(copy);
+  const scrollToExit = () => {
+    window.scrollTo(0, 0);
+    onExit();
   }
-};
 
   const { asPath } = useRouter();
   return (
@@ -50,7 +67,7 @@ const onLoad = () => {
       <AnimatePresence
         initial={true}
         exitBeforeEnter
-        onExitComplete={() => window.scrollTo(0, 0)}
+        onExitComplete={() => scrollToExit()}
       >
         <motion.div
           key={asPath}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,14 +2,12 @@ import "../styles/globals.css";
 import Header from "../components/header/header.js";
 import Footer from "../components/footer/footer.js";
 import Transition from "../components/page-ani";
-import Router from "next/router";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+
+
+
 
 function MyApp({ Component, pageProps }) {
-  
-
-
   const router = useRouter();
   return (
     <>
